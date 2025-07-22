e8a836cc13211494861664f04bd85ad2013a630e
IlMelons <melonss2002@gmail.com>
2025-07-22 15:32:25 +0200

    Moved to mnr_weapons

--- client/client.lua
+++ client/client.lua
@@ -1,6 +1,5 @@
 local hud = lib.load("config.client.hud")
 local idlecamera = lib.load("config.client.idlecamera")
-local pickups = lib.load("config.client.pickups")
 
 ---@description HUD COMPONENTS DISABLER
 for component, size in ipairs(hud) do
@@ -10,8 +9,3 @@ end
 ---@description IDLE CAMERA DISABLER
 DisableIdleCamera(idlecamera.afk)
 DisableVehiclePassengerIdleCamera(idlecamera.vehicle)
-
----@description PLAYER PICKUPS DISABLER
-for hash, toggle in ipairs(pickups) do
-    ToggleUsePickupsForPlayer(cache.playerId, hash, toggle)
-end
